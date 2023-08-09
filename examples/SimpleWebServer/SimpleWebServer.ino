5ea5e51f09639270c452d88385660e5e4a1fa9a1
rjjrbatarao <50077340+rjjrbatarao@users.noreply.github.com>
2023-08-09 22:40:41 +0800

    used asyncwebserver

--- examples/SimpleWebServer/SimpleWebServer.ino
+++ examples/SimpleWebServer/SimpleWebServer.ino
@@ -1,25 +1,39 @@
 
 #define LED_BUILTIN 2
 
+#if defined(ESP32)
 #include <WiFi.h>
-#include <Esp.h>
+#include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
+#include <ESPAsyncWebServer.h>
 #include <ImprovWiFiLibrary.h>
 
-WiFiServer server(80);
+AsyncWebServer server(80);
 ImprovWiFi improvSerial(&Serial);
 
 char linebuf[80];
 int charcount = 0;
 
+void handleWebIndexGet(AsyncWebServerRequest *request)
+{
+  AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html", "it works");
+  //response->addHeader("Cache-Control", "public, max-age=2678400"); // cache 31 days
+  request->send(response);  
+}
+
 void onImprovWiFiErrorCb(ImprovTypes::Error err)
 {
-  server.stop();
+  server.end();
   blink_led(2000, 3);
 }
 
 void onImprovWiFiConnectedCb(const char *ssid, const char *password)
 {
   // Save ssid and password here
+  server.on("/setup", HTTP_GET, handleWebIndexGet);
   server.begin();
   blink_led(100, 3);
 }
@@ -44,10 +58,10 @@ void setup()
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
 
-  improvSerial.setDeviceInfo(ImprovTypes::ChipFamily::CF_ESP32, "ImprovWiFiLib", "1.0.0", "BasicWebServer", "http://{LOCAL_IPV4}?name=Guest");
+  improvSerial.setDeviceInfo(ImprovTypes::ChipFamily::CF_ESP32, "ImprovWiFiLib", "1.0.0", "BasicWebServer", "http://{LOCAL_IPV4}/setup");
   improvSerial.onImprovError(onImprovWiFiErrorCb);
   improvSerial.onImprovConnected(onImprovWiFiConnectedCb);
-  improvSerial.setCustomConnectWiFi(connectWifi);  // Optional
+  improvSerial.setCustomConnectWiFi(connectWifi); 
 
   blink_led(100, 5);
 }
@@ -59,65 +73,7 @@ void loop()
 
   if (improvSerial.isConnected())
   {
-    handleHttpRequest();
-  }
-}
 
-void handleHttpRequest()
-{
-
-  WiFiClient client = server.available();
-  if (client)
-  {
-    blink_led(100, 1);
-    memset(linebuf, 0, sizeof(linebuf));
-    charcount = 0;
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    while (client.connected())
-    {
-      if (client.available())
-      {
-        char c = client.read();
-        // read char by char HTTP request
-        linebuf[charcount] = c;
-        if (charcount < sizeof(linebuf) - 1)
-          charcount++;
-
-        if (c == '\n' && currentLineIsBlank)
-        {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close"); // the connection will be closed after completion of the response
-          client.println();
-          client.println("<!DOCTYPE HTML><html><body>");
-          client.println("<h1 id=\"welcome\">Welcome!</h1>");
-          client.println("<p>This is a simple webpage served by your ESP32</p>");
-          client.println("<h3>Chip Info</h3>");
-          client.println("<ul><li>Model:");
-          client.println(ESP.getChipModel());
-          client.println("</li><li>Cores: ");
-          client.println(ESP.getChipCores());
-          client.println("</li><li>Revision: ");
-          client.println(ESP.getChipRevision());
-          client.println("</li></ul>");
-          client.println("<h3>Network Info</h3>");
-          client.println("<ul><li>SSID: ");
-          client.println(WiFi.SSID());
-          client.println("</li><li>IP Address: ");
-          client.println(WiFi.localIP());
-          client.println("</li><li>MAC Address: ");
-          client.println(WiFi.macAddress());
-          client.println("</li></ul>");
-          client.println("<script>const params = new URLSearchParams(document.location.search);document.getElementById('welcome').innerHTML = 'Welcome' + (params.get(\"name\") ? ', ' + params.get('name') : '') + '!';</script>");
-          client.println("</body></html>");
-          break;
-        }
-      }
-    }
-    delay(1);
-    client.stop();
   }
 }
 
